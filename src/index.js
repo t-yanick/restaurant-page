00c3001887e7a24ab8b87882818efabc34ef123b
Yanick <tyankee15@gmail.com>
2021-02-11 05:03:45 -0300

    Add logic for navigation of tabs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-alert('Testing the setup');
+import { pageLoad } from "./pageLoad";
+
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+const fufuEru = document.querySelector(".eru");
+
+fufuEru.addEventListener("click", () => {
+	document.querySelector("ul").classList.toggle("active");
+	fufuEru.classList.toggle("toggle");
+});
+
+tabs.forEach((tab) =>
+	tab.addEventListener("click", () => {
+		const target = document.querySelector(tab.dataset.tabTarget);
+		tabContents.forEach((tabContent) => {
+			tabContent.classList.remove("active");
+		});
+		tabs.forEach((tab) => {
+			tab.classList.remove("red");
+		});
+		tab.classList.add("red");
+		target.classList.add("active");
+	})
+);
