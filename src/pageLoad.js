77f5ef9c256392e016819982858226dd11e17776
Yanick <tyankee15@gmail.com>
2021-02-11 04:16:51 -0300

    Create home, menu, contact pages and add content to pageLoad.js file

---
+++ src/pageLoad.js
@@ -0,0 +1,21 @@
+const pageLoad = (() => {
+	const contentContainer = document.querySelector("#content");
+
+	const header = document.createElement("header");
+	header.innerHTML = `
+		<nav>
+			<h2>Fedel's Bar and Restaurant</h2>
+			<a href="#" class="eru">
+				<span class="line line1"></span>
+				<span class="line line2"></span>
+				<span class="line line3"></span>
+			</a>
+			<ul class="links">
+				<li data-tab-target="#home" class="tab red">Home</li>
+				<li data-tab-target="#home" class="tab">Menu</li>
+				<li data-tab-target="#home" class="tab">Contact Us</li>
+			</ul>
+		</nav>`;
+
+	contentContainer.appendChild(header)
+})();
