aff1d6b6739b3f6d7c7bc4c4afc3ca064151b037
Yanick <tyankee15@gmail.com>
2021-02-14 08:26:40 -0300

    Add css styling

--- src/homePage.js
+++ src/homePage.js
@@ -7,7 +7,7 @@ const renderHomePage = (() => {
     home.innerHTML = `
     <div id="home" class="active" data-tab-content>
         <div class="hero">
-            <h1>Visit Fedel's Restaurant for 100% Delicious Cameroonian Cuisines</h1>
+            <h1>Visit Fedel's Restaurant for 100% Delicious Traditional Cuisines</h1>
             <p>Very Tasty and Quite Affordable</p>
             <div class="btn-container">
                 <a data-tab-target="#menu" class="order-now">Order Now</a>
@@ -17,7 +17,7 @@ const renderHomePage = (() => {
             <div class="address">
                 <span><i class="fi-xnsuxl-map-marker-solid"></i></span>
                 <p>Carrefour Titus Edzoa, apres Carrefour Petites Genies<br />Simbock, Yaounde Cameroon</p>
-                <p>Tel: +237 677 495 016 or +237 696 905 999</p>
+               
             </div>
             <div class="hours">
                 <span><i class="fi-cnsuhl-clock-solid"></i></span>
