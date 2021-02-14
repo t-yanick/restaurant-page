57e2dde2f350c5c7b940b581a631d001a194d521
Yanick <tyankee15@gmail.com>
2021-02-14 05:16:57 -0300

    Add basic contact information

--- src/menuPage.js
+++ src/menuPage.js
@@ -17,6 +17,46 @@ const renderMenuPage = (() => {
 				</p>
 			</div>
 		</div>
+
+		<div class="menu-item">
+			<img src="./images/achu.jpeg" alt="" />
+			<div class="item-content">
+				<h4>Achu: <span>2,000XAF</span></h4>
+				<p>
+					Traditionally made Eru. Can be eaten with white garri, yellow garri or waterfufu
+				</p>
+			</div>
+		</div>
+
+		<div class="menu-item">
+			<img src="./images/ekwang.jpeg" alt="" />
+			<div class="item-content">
+				<h4>Ekwang: <span>2,000XAF</span></h4>
+				<p>
+					Traditionally made Eru. Can be eaten with white garri, yellow garri or waterfufu
+				</p>
+			</div>
+		</div>
+
+		<div class="menu-item">
+			<img src="./images/koki.jpeg" alt="" />
+			<div class="item-content">
+				<h4>Koki: <span>2,000XAF</span></h4>
+				<p>
+					Traditionally made Eru. Can be eaten with white garri, yellow garri or waterfufu
+				</p>
+			</div>
+		</div>
+
+		<div class="menu-item">
+			<img src="./images/pudding.jpeg" alt="" />
+			<div class="item-content">
+				<h4>Pudding: <span>2,000XAF</span></h4>
+				<p>
+					Traditionally made Eru. Can be eaten with white garri, yellow garri or waterfufu
+				</p>
+			</div>
+		</div>	
 	</div>
 </div>
 `;
